decf7cfbfda61a31ea8ef0533f3b9a42672a9ed7
Bayu Utomo <by.ciikel@gmail.com>
2020-02-29 15:03:05 +0700

    style(chat): add function history and create on chat channel

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -6,4 +6,16 @@ class ChatChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def history
+    messages = ChatChannel.all.collect(&:message)
+    data = { message: messages, type: 'history' }
+    ActionCable.server.broadcast('chat_channel', data)
+  end
+
+  def create(message)
+    result = ChatChannel.create(message: message.fetch('message'))
+    data = { message: result, type: 'create' }
+    ActionCable.server.broadcast('chat_channel', data)
+  end
 end
