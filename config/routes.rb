d1c86bfcc2df5a660d648d69b96ad25b54982296
Bayu Utomo <by.ciikel@gmail.com>
2020-02-29 15:28:46 +0700

    style(route): update info of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: proc { [200, {}, ['']] }
+  get '/', to: proc { [200, {}, ['Rails API runs in server']] }
   mount ActionCable.server => '/cable'
 end
